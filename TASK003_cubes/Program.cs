2a9c330988acd0db153238d40fd0e75efad0a6e8
Victor Rusaev <rusaev74@mail.ru>
2022-09-26 14:40:05 +0300

    TASK003_cubes

---
+++ TASK003_cubes/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+while(count <= N)
+{
+    Console.Write($"{Math.Pow(count, 3)} ");
+    count++;
+}
